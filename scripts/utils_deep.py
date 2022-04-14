0068e4ec30c2a574303ce01e9d4dd2a6b6d91237
nmningmei <semsociaty@gmail.com>
2022-04-14 13:23:01 +0200

    update function

--- scripts/utils_deep.py
+++ scripts/utils_deep.py
@@ -6,10 +6,36 @@ Created on Thu Apr 14 13:03:04 2022
 @author: nmei
 """
 
-def dataloader(dataset_name = 'CIFAR100'):
-    if dataset_name == 'CIFAR100':
-        
+import os,gc,torch
+
+from torchvision import datasets
+
+
+def dataloader(dataset_name:str = 'CIFAR100',
+               root:str = '../data',
+               train:bool = True,
+               transform = None,
+               target_transform = None,
+               download:bool = False,
+               ):
+    """
+    Download the datasets from PyTorch torchvision.datasets
+    If no dataset file exists, specific "download=True"
+    """
+    unpack = dict(root = root,
+                  train = train,
+                  transform = transform,
+                  target_transform = target_transform,
+                  download = download,
+                  )
+    if dataset_name == 'CIFAR100': # subset of CIFAR10
+        return datasets.CIFAR10(**unpack)
+    elif dataset_name == 'CIFAR10':
+        return datasets.CIFAR100(**unpack)
+    
 
 
 
 
+if __name__ == "__main__":
+    pass
--- scripts/vanilla_vae.py
+++ scripts/vanilla_vae.py
@@ -6,12 +6,13 @@ Created on Thu Apr 14 13:00:57 2022
 @author: nmei
 """
 
+from utils_deep import dataloader
 
 
 
 if __name__ == "__main__":
     
-    
+    dataloader_train = dataloader(dataset_name = 'CIFAR10',download = True,train = True)
 
 
 
